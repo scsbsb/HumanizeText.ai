21ea19f587efb3663fb52e73fb3b05b1612eb427
Sergio Santana <31173024+scsbsb@users.noreply.github.com>
2026-04-17 23:53:20 -0300

    activate github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // Define como relativo para funcionar em subpastas do GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
